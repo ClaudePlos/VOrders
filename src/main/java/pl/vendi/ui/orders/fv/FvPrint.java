7c3202554c326538be14bb1c4b259d487df01f51
Klaudiusz Skowronski <claude-plos@o2.pl>
2016-12-20 17:25:25 +0100

    k.skowronski add Print Invoice (PDF) v03

--- src/main/java/pl/vendi/ui/orders/fv/FvPrint.java
+++ src/main/java/pl/vendi/ui/orders/fv/FvPrint.java
@@ -49,16 +49,20 @@ public class FvPrint {
     protected Font font12;
     protected Font font12b;
     protected Font font14;
-    
+    protected Font font16b;
 
     
     public FvPrint() throws DocumentException, IOException
-    {    
-            font10 = new Font(FontFamily.HELVETICA, 10);
-            font10b = new Font(FontFamily.HELVETICA, 10, Font.BOLD);  
-            font12 = new Font(FontFamily.HELVETICA, 12);
-            font12b = new Font(FontFamily.HELVETICA, 12, Font.BOLD); 
-            font14 = new Font(FontFamily.HELVETICA, 14);
+    {
+            BaseFont helvetica = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
+            Font helvetica16=new Font(helvetica,16);
+        
+            font10  = new Font(helvetica, 10);
+            font10b = new Font(helvetica, 10, Font.BOLD);  
+            font12  = new Font(helvetica, 12);
+            font12b = new Font(helvetica, 12, Font.BOLD); 
+            font14  = new Font(helvetica, 14);
+            font16b = new Font(helvetica, 16, Font.BOLD);
     }
     
     
@@ -88,61 +92,86 @@ public class FvPrint {
                     document.open();
 
                     document.add(Chunk.NEWLINE);   //Something like in HTML :-)
-
-                    //document.add(new Paragraph("TEST" + order.getOwnNumber() ));
                     
-             
+                    Paragraph numInvoice = new Paragraph( order.getOwnNumber().replace("ZWD", "FV"), font16b  );
+                    numInvoice.setAlignment(Element.ALIGN_CENTER);
+                    document.add( numInvoice );
+                    
+                    document.add(Chunk.NEWLINE);
+                    
                     
                     
                      // Address seller / buyer
                     PdfPTable table = new PdfPTable(2);
                     table.setWidthPercentage(100);
-                    PdfPCell seller = getPartyAddress("From:",
+                    PdfPCell seller = getPartyAddress("Sprzedawca:",
                             order.getSupplier().getName(), 
                             order.getSupplier().getCity(),
                             order.getSupplier().getNip(),
                             order.getSupplier().getAddress(),
-                            order.getSupplier().getNip(),
-                            order.getSupplier().getExternalId() );
+                            "",
+                            "" );
                     table.addCell(seller);
-                    PdfPCell buyer = getPartyAddress("To:",
+                    PdfPCell buyer = getPartyAddress("Nabywca:",
                             order.getClient().getName(), 
                             order.getClient().getCity(),
                             order.getClient().getNip(),
                             order.getClient().getAddress(),
-                            order.getClient().getNip(),
-                            order.getClient().getExternalId() );
+                            "",
+                            "" );
                     table.addCell(buyer);
                  
                   
                     document.add(table);
+                    
+                    
+                    document.add(Chunk.NEWLINE);
+                    
 
                     // line items
-                    table = new PdfPTable(6);
+                    table = new PdfPTable(7);
                     table.setWidthPercentage(100);
                     table.setSpacingBefore(10);
                     table.setSpacingAfter(10);
-                    table.setWidths(new int[]{7, 2, 1, 2, 2, 2});
-                    table.addCell(getCell("Item:", Element.ALIGN_LEFT, font12b));
-                    table.addCell(getCell("Price:", Element.ALIGN_LEFT, font12b));
-                    table.addCell(getCell("Qty:", Element.ALIGN_LEFT, font12b));
-                    table.addCell(getCell("Subtotal:", Element.ALIGN_LEFT, font12b));
-                    table.addCell(getCell("VAT:", Element.ALIGN_LEFT, font12b));
-                    table.addCell(getCell("Total:", Element.ALIGN_LEFT, font12b));
+                    table.setWidths(new int[]{7, 2, 2, 1, 2, 2, 2});
+                    table.addCell(getCell("Pozycja", Element.ALIGN_LEFT, font12b));
+                    table.addCell(getCell("Cena", Element.ALIGN_LEFT, font12b));
+                    table.addCell(getCell("Ilość", Element.ALIGN_LEFT, font12b));
+                    table.addCell(getCell("jm", Element.ALIGN_LEFT, font12b));
+                    table.addCell(getCell("Netto", Element.ALIGN_LEFT, font12b));
+                    table.addCell(getCell("VAT", Element.ALIGN_LEFT, font12b));
+                    table.addCell(getCell("Brutto", Element.ALIGN_LEFT, font12b));
                    
                     for ( DocumentItem di : order.getItems() ) {
                      
                         table.addCell(getCell( di.getProduct().getName(), Element.ALIGN_LEFT, font12));
+                        table.addCell(getCell( di.getUnitPriceNet().toString(), Element.ALIGN_RIGHT, font12));
+                        table.addCell(getCell( di.getAmount().toString(), Element.ALIGN_RIGHT, font12));
+                        table.addCell(getCell( di.getProduct().getMeasureUnit().getAbbr(), Element.ALIGN_RIGHT, font12));
                         table.addCell(getCell( di.getValueNet().toString(), Element.ALIGN_RIGHT, font12));
                         table.addCell(getCell( di.getValueTax().toString(), Element.ALIGN_RIGHT, font12));
-                        table.addCell(getCell( di.getValueBrut().toString(), Element.ALIGN_RIGHT, font12));
-                        table.addCell(getCell( di.getValueBrut().toString(), Element.ALIGN_RIGHT, font12));
-                        table.addCell(getCell(
-                            di.getValueBrut().toString() ,
-                            Element.ALIGN_RIGHT, font12));
+                        table.addCell(getCell( di.getValueBrut().toString() , Element.ALIGN_RIGHT, font12));
                     }
                     document.add(table);
                     
+                    
+                    
+                    
+                 String[] t = new String[20];;
+                 t[1] = ":sdf";
+                // grand totals
+                
+                PdfPTable t2 = getTotalsTable(
+                "123", "124", "125", "126",
+                t[1], t[1],
+                t[1], t[1], t[1] );
+                
+                
+                document.add( t2
+                        );
+ 
+                // payment info
+                //document.add(getPaymentInfo(basic.getPaymentReference(), basic.getPaymentMeansPayeeFinancialInstitutionBIC(), basic.getPaymentMeansPayeeAccountIBAN()));
                     
                     
 
@@ -183,8 +212,8 @@ public class FvPrint {
         cell.addElement(new Paragraph(who, font12b));
         cell.addElement(new Paragraph(name, font12));
         cell.addElement(new Paragraph(line1, font12));
-        cell.addElement(new Paragraph(line2, font12));
-        cell.addElement(new Paragraph(String.format("%s-%s %s", countryID, postcode, city), font12));
+        cell.addElement(new Paragraph("NIP: " + line2, font12));
+        cell.addElement(new Paragraph(String.format("%s %s %s", countryID, postcode, city), font12));
         return cell;
     }
     
@@ -199,5 +228,40 @@ public class FvPrint {
     }
    
   
+    
+    public PdfPTable getTotalsTable(String tBase, String tTax, String tTotal, String tCurrency,
+            String[] type, String[] percentage, String base[], String tax[], String currency[]) throws DocumentException {
+        PdfPTable table = new PdfPTable(6);
+        table.setWidthPercentage(100);
+        table.setWidths(new int[]{1, 1, 3, 3, 3, 1});
+        table.addCell(getCell("TAX", Element.ALIGN_LEFT, font12b));
+        table.addCell(getCell("%", Element.ALIGN_RIGHT, font12b));
+        table.addCell(getCell("Base amount:", Element.ALIGN_LEFT, font12b));
+        table.addCell(getCell("Tax amount:", Element.ALIGN_LEFT, font12b));
+        table.addCell(getCell("Total:", Element.ALIGN_LEFT, font12b));
+        table.addCell(getCell("", Element.ALIGN_LEFT, font12b));
+        int n = type.length;
+        for (int i = 0; i < n; i++) {
+            table.addCell(getCell(type[i], Element.ALIGN_RIGHT, font12));
+            table.addCell(getCell(percentage[i], Element.ALIGN_RIGHT, font12));
+            table.addCell(getCell(base[i], Element.ALIGN_RIGHT, font12));
+            table.addCell(getCell(tax[i], Element.ALIGN_RIGHT, font12));
+            double total = Double.parseDouble(base[i]) + Double.parseDouble(tax[i]);
+            table.addCell(getCell( "ksTODO" , Element.ALIGN_RIGHT, font12));
+            table.addCell(getCell(currency[i], Element.ALIGN_LEFT, font12));
+        }
+        PdfPCell cell = getCell("", Element.ALIGN_LEFT, font12b);
+        cell.setColspan(2);
+        cell.setBorder(PdfPCell.NO_BORDER);
+        table.addCell(cell);
+        table.addCell(getCell(tBase, Element.ALIGN_RIGHT, font12b));
+        table.addCell(getCell(tTax, Element.ALIGN_RIGHT, font12b));
+        table.addCell(getCell(tTotal, Element.ALIGN_RIGHT, font12b));
+        table.addCell(getCell(tCurrency, Element.ALIGN_LEFT, font12b));
+        return table;
+    }
+    
+    
+    
     
 }
